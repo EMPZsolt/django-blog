8a209db83855f72ed8de57ce470d3f7993611054
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2024-01-12 15:31:28 +0000

    Ready for deploy the about app

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-empzsolt-djangoblog-8kuej1884w3.ws-eu107.gitpod.io',
                 '.herokuapp.com',]
